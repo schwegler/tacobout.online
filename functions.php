74179fefb57af6aa6897c73095d3872d39cf87db
Andrew Schwegler <schwegler@gmail.com>
2026-06-24 13:58:39 -0500

    fix: update pagination regex and move header blur to pseudo-element to resolve mobile menu clipping and badge styling

--- functions.php
+++ functions.php
@@ -132,7 +132,7 @@ add_action( 'after_setup_theme', 'tacobout_clear_saved_templates' );
 function tacobout_pagination_body_class( $classes ) {
 	// Check for query block pagination params
 	foreach ( $_GET as $key => $value ) {
-		if ( preg_match( '/^query-\d+-page$/', $key ) && intval( $value ) > 1 ) {
+		if ( preg_match( '/^query(?:-\d+)?-page$/', $key ) && intval( $value ) > 1 ) {
 			$classes[] = 'paged';
 			break;
 		}
